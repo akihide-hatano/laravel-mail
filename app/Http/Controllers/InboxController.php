7052a0b84b9752988d1c0fb13b847a04f0d5aa3c
akihide-hatano <a.h.19841202@gmail.com>
2025-09-29 09:29:27 +0900

    InboxController.phpを更新

--- app/Http/Controllers/InboxController.php
+++ app/Http/Controllers/InboxController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreInboxRequest;
+use App\Models\MailInbox;
 
 class InboxController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = MailInbox::query()->latest('received_at')->latest()->paginate(10);
+        return view('mail.inbox', compact('items'));
+    }
+
+    // 疑似受信
+    public function receive(StoreInboxRequest $request)
+    {
+        $data = $request->validated();
+
+        MailInbox::create([
+            'from_email'  => $data['from_email'],
+            'subject'     => $data['subject'] ?? null,
+            'body'        => $data['body'] ?? null,
+            'received_at' => now(),
+            'is_read'     => false,
+        ]);
+
+        return back()->with('ok', '受信BOXに保存しました');
+    }
+
+    public function toggleRead(MailInbox $inbox)
+    {
+        $inbox->is_read = ! $inbox->is_read;
+        $inbox->save();
+
+        return back()->with('ok', '既読フラグを切り替えました');
+    }
 }
