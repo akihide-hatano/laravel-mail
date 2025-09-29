b824473c9fef7f80ec93c0e2e7caa8748903cc25
akihide-hatano <a.h.19841202@gmail.com>
2025-09-30 07:45:53 +0900

    Controllers/Mailに集約 views/mailをinbox, outboxに分割

---
+++ app/Http/Controllers/Mail/OutboxController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Mail;
+
+use App\Http\Controllers\Controller;
+use App\Models\MailOutbox;
+use Illuminate\Http\Request;
+
+class OutboxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MailOutbox $mailOutbox)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MailOutbox $mailOutbox)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MailOutbox $mailOutbox)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MailOutbox $mailOutbox)
+    {
+        //
+    }
+}
