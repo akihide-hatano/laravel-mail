0b175324fc20a502012c3a4e45f3f33be957369d
akihide-hatano <a.h.19841202@gmail.com>
2025-09-29 15:36:05 +0900

    Models/MailInbox.phpを修正

--- app/Models/MailInbox.php
+++ app/Models/MailInbox.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MailInbox extends Model
 {
     /** @use HasFactory<\Database\Factories\MailInboxFactory> */
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'mail_inbox';
     protected $fillable = [
