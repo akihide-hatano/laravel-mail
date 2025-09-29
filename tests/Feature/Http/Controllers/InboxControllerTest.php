8cfef3c59d42c669f23d5811c593fd722ad6f585
akihide-hatano <a.h.19841202@gmail.com>
2025-09-30 08:51:10 +0900

    InboxControllerTestを修正

--- tests/Feature/Http/Controllers/InboxControllerTest.php
+++ tests/Feature/Http/Controllers/InboxControllerTest.php
@@ -5,12 +5,16 @@ namespace Tests\Feature\Http\Controllers;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\MailInbox;
+use App\Models\User;
 
 class InboxControllerTest extends TestCase
 {
     /**
      * A basic feature test example.
-     */
+    */
+    use RefreshDatabase;
+
     public function test_example(): void
     {
         $response = $this->get('/');
