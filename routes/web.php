a8f22002add75b8426fecc766f16c049c747eb1f
akihide-hatano <a.h.19841202@gmail.com>
2025-09-29 09:09:29 +0900

    web.phpを編集

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OutboxController;
+use App\Http\Controllers\InboxController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->prefix('mail')->name('mail.')->group(function () {
+    // Outbox (index, store, destroyを定義)
+    Route::resource('outbox', OutboxController::class)->only([
+        'index', 'store', 'destroy'
+    ]);
+
+    // Inbox (indexを定義)
+    Route::resource('inbox', InboxController::class)->only([
+        'index'
+    ]);
+
+    // Inboxのカスタムアクション
+    // Route::resourceの外に定義することで、カスタムアクションとして維持
+    Route::post('/inbox/receive', [InboxController::class, 'receive'])->name('inbox.receive'); // 疑似受信
+    Route::patch('/inbox/{inbox}/toggle-read', [InboxController::class, 'toggleRead'])->name('inbox.toggle');
 });
 
 require __DIR__.'/auth.php';
