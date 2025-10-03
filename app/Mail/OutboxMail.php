ece665ffd80a716b31f62e218fabab783e6c9c64
akihide-hatano <a.h.19841202@gmail.com>
2025-10-03 12:03:08 +0900

    mail/OutboxMail.phpを修正

---
+++ app/Mail/OutboxMail.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\MailOutbox;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OutboxMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public MailOutbox $outbox)
+    {
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->outbox->subject ?? '(no subject)',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.outbox',
+            with:['outbx'=>$this->outbox];
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
