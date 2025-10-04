cd3ac29cb6b4b61ec91e7aa0f796ed6c13548731
akihide-hatano <a.h.19841202@gmail.com>
2025-10-04 10:52:44 +0900

    outbox/show.blade.phpのbuttonのリッチなスタイルに変更

--- resources/views/outbox/show.blade.php
+++ resources/views/outbox/show.blade.php
@@ -7,16 +7,29 @@
         <dt class="font-semibold col-span-1">宛先</dt><dd class="col-span-3">{{ $outbox->to_email }}</dd>
         <dt class="font-semibold col-span-1">件名</dt><dd class="col-span-3">{{ $outbox->subject }}</dd>
         <dt class="font-semibold col-span-1">本文</dt><dd class="col-span-3 whitespace-pre-wrap">{{ $outbox->body }}</dd>
-        <dt class="font-semibold col-span-1">ステータス</dt><dd class="col-span-3 status->colorClasses()">{{ $outbox->status->label() }}</dd>
+        <dt class="font-semibold col-span-1">ステータス</dt>
+        <dd class="col-span-3 ">  
+          <span class="inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium ring-1 whitespace-nowrap {{ $outbox->status->colorClasses() }}">
+            {{ $outbox->status->label() }}
+          </span></dd>
         <dt class="font-semibold col-span-1">登録</dt><dd class="col-span-3">{{ optional($outbox->created_at)->format('Y-m-d H:i') }}</dd>
       </dl>
     </div>
 
-    <div class="flex gap-2">
-      <a href="{{ route('mail.outbox.index') }}" class="underline">← 一覧へ</a>
-      <form method="POST" action="{{ route('mail.outbox.destroy', $outbox) }}" onsubmit="return confirm('削除しますか？')">
-        @csrf @method('DELETE')
-        <button class="text-red-600 underline">削除</button>
+    <div class=" flex gap-2">
+      <a href="{{route('mail.outbox.index')}}" class="inline-flex items-center px-3 py-2 rounded ring-1 ring-gray-600 hover:bg-gray-100">
+        一覧へ
+      </a>
+
+      @if($outbox->status === \App\Enums\OutboxStatus::Draft )
+          <a href="{{ route('mail.outbox.edit',$outbox) }}" class="inline-flex items-center px-3 py-2 rounded bg-blue-600 text-white hover:bg-blue-500">
+              編集
+          </a>
+      @endif
+
+      <form action="POST" action="{{route('mail.outbox.destroy',$outbox)}}" onsubmit="return confirm('削除しますか?')">
+          @csrf @method('DELETE')
+          <button class="inline-flex items-center px-3 py-2 rounded bg-rose-600 text-white hover:bg-rose-500">削除</button>
       </form>
     </div>
   </div>
